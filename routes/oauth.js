79ea0efb65e41ac3a8349b7cf6333a8f600934a6
Nico Wehmöller <nico.wehmoeller@eripini.win>
2018-01-13 16:06:20 +0100

    added user template

--- routes/oauth.js
+++ routes/oauth.js
@@ -3,24 +3,24 @@ var steem = require('../utils/steem')
 let utopian = require('utopian-api');
 var router = express.Router();
 
-router.get("/t",(req,res) => {
+router.get("/t", (req, res) => {
 
     res.send("hi")
 })
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     if (!req.query.access_token) {
         let uri = steem.getLoginURL();
         res.redirect(uri);
     } else {
         steem.setAccessToken(req.query.access_token);
-        steem.me(function(err, response) {
+        steem.me(function (err, response) {
             let mod = utopian.getModerator(response.user);
             mod.then((mod) => {
                 console.log(mod)
                 if (mod.length === 1 && mod[0].account === response.user) {
-                    if (mod.supermoderator || response.user === "wehmoen") {
+                    if (response.user === "wehmoen") {
                         response.account.json_metadata = JSON.parse(response.account.json_metadata);
                         response.access_token = req.query.access_token;
                         response.utopian = {
@@ -28,10 +28,18 @@ router.get('/', function(req, res, next) {
                         };
                         req.session.steem = response;
                         res.redirect("/v")
-
                     } else {
-                        req.session.noMod = response.user;
-                        res.redirect('/?noMod=true')
+                        if (!mod.supermoderator) {
+                            req.session.noMod = response.user;
+                            res.redirect('/?noMod=true')
+                        } else {
+                            response.account.json_metadata = JSON.parse(response.account.json_metadata);
+                            response.access_token = req.query.access_token;
+                            response.utopian = {
+                                user: mod
+                            };
+                            req.session.steem = response;
+                        }
                     }
                 } else {
                     req.session.noMod = response.user;
@@ -43,8 +51,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/destroy', (req, res) => {
-   req.session.destroy();
-   res.redirect("/")
+    req.session.destroy();
+    res.redirect("/")
 });
 
 module.exports = router;
