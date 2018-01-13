97e3dadfeecf67d284a68614ad29b068827ca012
Nico Wehmöller <nico.wehmoeller@eripini.win>
2018-01-13 21:45:22 +0100

    added accepted and rejected posts

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -15,7 +15,8 @@ function getAcceptedPosts(username, skip, limit) {
         url: "/api/posts/accepted/" + username +".json?skip="+skip+"&limit="+limit,
         success: function(response) {
             response.results.forEach(function(post) {
-                $('#accepted_posts').append('<tr><td>'+post.author+'</td><td>'+post.category+'</td><td>'+post.title+'</td><td><a href="//utopian.io'+post.url+'" class="btn btn-default btn-sm">View Post</a></td></tr>')
+                var created = moment.utc(new Date(post.created)).from(moment.utc().format('YYYY-MM-DD HH:mm:ss'));
+                $('#accepted_posts').append('<tr><td>'+post.author+'</td><td>'+post.category+'</td><td>'+post.title+'</td><td>'+created+'</td><td><a target="_blank" href="//utopian.io'+post.url+'" class="btn btn-default btn-sm">View Post</a></td></tr>')
             })
         }
     })
@@ -25,7 +26,8 @@ function getRejectedPosts(username, skip, limit) {
         url: "/api/posts/hidden/" + username +".json?skip="+skip+"&limit="+limit,
         success: function(response) {
             response.results.forEach(function(post) {
-                $('#rejected_posts').append('<tr><td>'+post.author+'</td><td>'+post.category+'</td><td>'+post.title+'</td><td><a href="//utopian.io'+post.url+'" class="btn btn-default btn-sm">View Post</a></td></tr>')
+                var created = moment.utc(new Date(post.created)).from(moment.utc().format('YYYY-MM-DD HH:mm:ss'));
+                $('#rejected_posts').append('<tr><td>'+post.author+'</td><td>'+post.category+'</td><td>'+post.title+'</td><td>'+created+'</td><td><a target="_blank" href="//utopian.io'+post.url+'" class="btn btn-default btn-sm">View Post</a></td></tr>')
             })
         }
     })
