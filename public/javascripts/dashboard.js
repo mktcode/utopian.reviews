0392789c82a43e67585d29b1fa379715c32e9273
Nico Wehmöller <nico.wehmoeller@eripini.win>
2018-01-13 18:59:12 +0100

    added accepted and rejected posts

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -9,3 +9,26 @@ function updateModeratorStatistic(moderator) {
         }
     })
 }
+
+function getAcceptedPosts(username, skip, limit) {
+    $.ajax({
+        url: "/api/posts/accepted/" + username +".json?skip="+skip+"&limit="+limit,
+        success: function(response) {
+            response.results.forEach(function(post) {
+                $('#accepted_posts').append('<tr><td>'+post.author+'</td><td>'+post.category+'</td><td>'+post.title+'</td><td><a href="//utopian.io'+post.url+'" class="btn btn-default btn-sm">View Post</a></td></tr>')
+            })
+        }
+    })
+}
+function getRejectedPosts(username, skip, limit) {
+    $.ajax({
+        url: "/api/posts/hidden/" + username +".json?skip="+skip+"&limit="+limit,
+        success: function(response) {
+            response.results.forEach(function(post) {
+                $('#rejected_posts').append('<tr><td>'+post.author+'</td><td>'+post.category+'</td><td>'+post.title+'</td><td><a href="//utopian.io'+post.url+'" class="btn btn-default btn-sm">View Post</a></td></tr>')
+            })
+        }
+    })
+}
+
+
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 let utopian = require('utopian-api');
 let moment = require('moment');
+let rq = require('request');
 var router = express.Router();
 
 /* GET users listing. */
@@ -22,30 +23,32 @@ router.get('/moderator.json', (req, res) => {
     }
 });
 
-router.get('/urgent.json', (req, res) => {
-    if (!req.query.account) {
-        res.json({count:0,results:[]});
+router.get('/posts/:status/:user.json', (req,res) => {
+
+    let status = req.params.status;
+    if (req.query.skip) {
+        skip = parseInt(req.query.skip);
+        if (skip < 0 || skip > 50) {
+            skip = 20;
+        }
     } else {
-
-        utopian.getPendingPostsByModerator(req.query.account).then((result) => {
-            let posts = [];
-            if (result.results.length === 0) {
-                res.json({count:0,results:[]});
-            } else {
-                result.results.forEach((post) => {
-                    var duration = moment.duration(moment().diff(post.created));
-                    var hours = duration.asDays();
-                    if (parseInt(hours) >= 4) {
-                        posts.push(post);
-                    }
-                });
-                res.json({total:posts.length,results:posts})
-            }
-        })
-
+        skip = 0;
     }
-});
+    let limit = parseInt(req.query.limit);
 
+    if (limit < 0 || limit > 50) {
+        limit = 20;
+    }
+
+    if (status !== "accepted" && status !== "hidden") {
+        next();
+    } else {
+        rq("https://utopian.plus/posts/"+status+"/"+req.params.user+".json?skip="+skip+"&limit="+limit, (err,result,body) => {
+            res.json(JSON.parse(body));
+        })
+    }
+
+});
 
 
 module.exports = router;
